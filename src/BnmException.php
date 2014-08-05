9a3ef6b44a76fc2ef670d6ec85cfa1d52b924945
Королёв Олег <coroliov.o@fruitware.ru>
2014-08-06 00:36:31 +0300

    first commit

---
+++ src/BnmException.php
@@ -0,0 +1,14 @@
+<?php
+namespace Fruitware\Bnm;
+
+use Exception;
+
+/**
+ * Bnm exception class
+ * Class BnmException
+ * @package Fruitware\Bnm
+ */
+class BnmException extends Exception
+{
+
+}
